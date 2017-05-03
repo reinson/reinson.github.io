e2916da83c237c4f08d975bc425bde003451d45a
Tormi Reinson <tormi.reinson@gmail.com>
2017-05-04 01:00:47 +0300

    New video files and project descriptions

--- js/script.js
+++ js/script.js
@@ -1,47 +1,47 @@
 var projects = {};
 projects.uefa = {
-    videoSource: "img/uefa.ogv",
+    videoSource: "mp4/uefa.mp4",
     title: "UEFA Euro 2016 map",
     link: "http://euro2016map.appspot.com",
     visibleLink:  "euro2016map.appspot.com",
     tags: ["D3.js", "JavaScript", "Google maps API", "Python"],
-    description: "This is an interactive visualization that shows where are the home clubs of footballers from the UEFA Euro2016 competition. Select a country and it's players are visualized on the map.  Made as a personal project with a friend. I did most of the front end work and she scraped and geolocated the player's data."
+    description: "As a football fan I decided to create this application to visualize where are the players of different national teams playing club football. Select a country competing in UEFA European Championships 2016 and its players' home club locations are visualized on the map.  Made as a personal project with a friend. I did most of the front end work and she scraped and geolocated the player's data."
 };
 
 projects.dgns = {
-    videoSource: "img/dgns.ogv",
+    videoSource: "mp4/stacked-area.mp4",
     title: "Overview of the Estonian specialized medical care spending",
     link: "https://www.stacc.ee/ehif-stacked-area/?lng=En",
     visibleLink: "stacc.ee/ehif-stacked-area",
     tags: ["D3.js","JavaScript","Data analysis", "Python", "SQL"],
-    description: "This is an interactive data visualization of Estonian specialised medical care costs. It is based on the Estonian Health Insurance Fund (EHIF) data. EHIF is the only health insurance provider in Estonia. Health care bills were analyzed based on the main diagnoses and patients' age."
+    description: "<p>This is an interactive data visualization of Estonian specialised medical care costs. It is based on the Estonian Health Insurance Fund (EHIF) data. Health care bills were analyzed based on the main diagnoses and patients' age. </p><p>It received a lot of positive feedback through traditional <a href='http://tervis.postimees.ee/3930967/vaata-ise-jarele-kelle-peale-tegelikult-kulub-loviosa-ravirahast'>[1]</a> <a href='http://m.tervist24.ohtuleht.ee/774191/eriarstiabile-tehtud-kulutusi-saab-nuudsest-jalgida-interaktiivselt'>[2]</a> and social media and therefore turned out to be a success  for my employer (STACC) and the client (Estonian Health Insurance Fund).</p>"
 };
 
 projects.demographics = {
-    videoSource: "img/demographics.ogv",
+    videoSource: "mp4/demographics.mp4",
     title: "Demographics of Estonia",
     link: "http://bl.ocks.org/reinson/raw/4edb6d60afd451e1cfaf6369f943bd7b/",
     visibleLink: "Link to visualization",
     tags: ["D3.js", "JavaScript"],
-    description: "A personal visualization project I made to practice D3.js. It shows demographic changes of Estonia from the beginning of 20th century up to year 2040 with 25 year forecast."
+    description: "Estonia is facing a demographic crisis with decreasing birth rates and increasing number of elderly people. I visualized demographic changes of Estonia from the beginning of 20th century up to year 2040 with 25 year forecast to give an overview of the data that describes the serious problem."
 };
 
 projects.googleMaps = {
-    videoSource: "img/googleMaps.ogv",
+    videoSource: "mp4/google-maps.mp4",
     title: "Estonian cities Google maps app",
     link: "http://reinson.github.io/Udacity_WDND_final_project/index.html",
     visibleLink: "Link to visualization",
     tags: ["D3.js", "Knockout.js", "Google maps API", "Wikipedia API", "JavaScript", "Udacity"],
-    description: "This Google maps based visualization shows all the cities and towns of Estonia. It uses Knocokout.js to populate and filter cities list in the sidebar and Wikipedia API to provide descriptions about the places."
+    description: "Since there is an administrative reform going on right now in Estonia I choose to visualize some Estonian geograpichal data as a project for the Udacity Front End Nanodegree. This Google maps based visualization shows all the cities and towns of Estonia. It uses Knocokout.js to populate and filter cities list in the sidebar and Wikipedia API to provide descriptions about the places."
 };
 
 projects.ehifBudget = {
-    videoSource: "img/ehifBudget.ogv",
+    videoSource: "mp4/ehif-budget.mp4",
     title: "Budget of Estonian Health Insurance Fund",
     link: "https://www.stacc.ee/ehif-budget/",
     visibleLink: "Link to visualization",
     tags: ["D3.js", "HTML", "JavaScript"],
-    description: ""
+    description: "The goal of this project was to give an overview of the budget of Estonian Health Insurance Fund. It shows data about both income and expense sides of the budget. It includes an interactive treemap visualization of expense hierarchy that I am quite pleased with."
 };
 
 projects.arcadeGame = {
@@ -54,12 +54,12 @@ projects.arcadeGame = {
 };
 
 projects.election = {
-    videoSource: "",
+    videoSource: "mp4/elections.mp4",
     title: "Estonian parliamentary election visualization",
     link: "http://bl.ocks.org/reinson/raw/740e25faa7942f25896f/",
     visibleLink: "Link to visualization",
     tags: ["JavaScript", "D3.js", "Udacity"],
-    description: "D3.js based interactive bubble chart that shows the profile of a typical electronic (over the internet) voter. Viewer can change the x-axis and filter data, which results in animated transitions in bubble locations and axes."
+    description: "Estonia is one of the first countries to use E-voting (over the internet voting) for general election. This D3.js based interactive bubble chart shows the profile of a typical electronic voter by allowing the viewer to explore how the fraction of electronic votes depends on different characteristics of candidates."
 };
 
 projects.phd =  {
@@ -68,7 +68,7 @@ projects.phd =  {
     link: "https://dspace.ut.ee/bitstream/handle/10062/49900/reinson_tormi.pdf?sequence=1&isAllowed=y",
     visibleLink: "Link to thesis",
     tags: ["Science", "Data analysis", "Data visualization", "Public speaking", "Writing"],
-    description: ""
+    description: "<p>I have a PhD degree in molecular virology. I studied Human Papillomavirus and clarified some important aspects of its DNA replication that can play an important part in causing cancer.</p><p>  During my studies I published <a href='https://www.ncbi.nlm.nih.gov/pubmed?term=(reinson%20t)%20AND%20(%222011%2F01%2F01%22%5BDate%20-%20Publication%5D%20%3A%20%222016%2F01%2F01%22%5BDate%20-%20Publication%5D)'>four scientific articles</a> and gave several presentations in international confernces. I performed and analyzed tons of experiments and wrote up the results.</p><p> It was an interesting job and invaluable experience.</p> "
 };
 
 projects.wines = {
@@ -95,7 +95,7 @@ projects.firstNeuralNet = {
     link: "https://github.com/reinson/deep-learning/blob/master/first-neural-network/DLND%20Your%20first%20neural%20network.ipynb",
     visibleLink: "Link to project",
     tags: ["Machine learning", "Deep learning", "Udacity"],
-    description: ""
+    description: "My first neural network that predicts daily bike rental ridership. I made this project during my ongoing Deep Learning Foundations nanodegree program, which is already my third nanodegree from Udacity."
 };
 
 
