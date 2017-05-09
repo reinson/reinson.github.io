b78d11d41bdaf743d26e5b265b4b8faf76a058b1
Tormi Reinson <tormi.reinson@gmail.com>
2017-05-09 22:31:24 +0300

    Changes in the project descriptions

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ projects.uefa = {
     link: "http://euro2016map.appspot.com",
     visibleLink:  "euro2016map.appspot.com",
     tags: ["D3.js", "JavaScript", "Google maps API", "Python"],
-    description: "As football fans we created this application to provide some interesting insights into teams competing in the last UEFA European Championship. By selecting a country its players' home club locations are drawn on the map. Made as a personal project with a friend. I did most of the front end work and she scraped and geolocated the player's data."
+    description: "As a football fan I created, together with a friend, this application to provide interesting insights into teams competing in the last UEFA European Championship. By selecting a country its players' home club locations are drawn on the map. I did most of the front end work and the friend scraped and geolocated the player's data."
 };
 
 projects.dgns = {
@@ -14,7 +14,7 @@ projects.dgns = {
     link: "https://www.stacc.ee/ehif-stacked-area/?lng=En",
     visibleLink: "stacc.ee/ehif-stacked-area",
     tags: ["D3.js","JavaScript","Data analysis", "Python", "SQL"],
-    description: "<p>This is an interactive data visualization of Estonian specialised medical care costs. It is based on the Estonian Health Insurance Fund (EHIF) data. Health care bills were analyzed based on the main diagnoses and patients' age. </p><p>It received a lot of positive feedback through traditional <a href='http://tervis.postimees.ee/3930967/vaata-ise-jarele-kelle-peale-tegelikult-kulub-loviosa-ravirahast'>[1]</a> <a href='http://m.tervist24.ohtuleht.ee/774191/eriarstiabile-tehtud-kulutusi-saab-nuudsest-jalgida-interaktiivselt'>[2]</a> and social media and therefore turned out to be a success  for my employer (STACC) and the client (Estonian Health Insurance Fund).</p>"
+    description: "<p>This interactive data visualization of Estonian specialised medical care costs is based on the Estonian Health Insurance Fund (EHIF) data. Health care bills were analyzed based on the main diagnoses and patients' age. </p><p>The analysis and visualization received a lot of positive feedback through traditional <a href='http://tervis.postimees.ee/3930967/vaata-ise-jarele-kelle-peale-tegelikult-kulub-loviosa-ravirahast'>[1]</a> <a href='http://m.tervist24.ohtuleht.ee/774191/eriarstiabile-tehtud-kulutusi-saab-nuudsest-jalgida-interaktiivselt'>[2]</a> and social media and therefore turned out to be a success  for my employer (STACC) and the client (Estonian Health Insurance Fund).</p>"
 };
 
 projects.demographics = {
@@ -32,7 +32,7 @@ projects.googleMaps = {
     link: "http://reinson.github.io/Udacity_WDND_final_project/index.html",
     visibleLink: "Link to visualization",
     tags: ["D3.js", "Knockout.js", "Google maps API", "Wikipedia API", "JavaScript", "Udacity"],
-    description: "Since there is an administrative reform going on in Estonia I choose to visualize some Estonian geograpichal data as a project for the Udacity Front End Development Nanodegree. This Google maps based visualization shows all the cities and towns of Estonia. It uses Knocokout.js to populate and filter cities list in the sidebar and Wikipedia API to provide descriptions about the places."
+    description: "Since there is an administrative reform going on in Estonia I chose to visualize Estonian geograpichal data as a project for the Udacity Front End Development Nanodegree. This Google maps based visualization shows all the cities and towns of Estonia. It uses Knockout.js to populate and filter the cities list in the sidebar and Wikipedia API to provide descriptions about the places."
 };
 
 projects.ehifBudget = {
@@ -41,7 +41,7 @@ projects.ehifBudget = {
     link: "https://www.stacc.ee/ehif-budget/",
     visibleLink: "Link to visualization",
     tags: ["D3.js", "HTML", "JavaScript"],
-    description: "The goal of this project was to give an overview of the budget of Estonian Health Insurance Fund. It shows data about both income and expense sides of the budget. It includes an interactive treemap visualization of expense hierarchy that I am quite pleased with."
+    description: "The goal of this project was to give an overview of the budget of Estonian Health Insurance Fund. The project visualises both the income and expense sides of the budget. It includes an interactive treemap visualization of expense hierarchy that I am especially pleased with."
 };
 
 projects.arcadeGame = {
@@ -59,7 +59,7 @@ projects.election = {
     link: "http://bl.ocks.org/reinson/raw/740e25faa7942f25896f/",
     visibleLink: "Link to visualization",
     tags: ["JavaScript", "D3.js", "Udacity"],
-    description: "Estonia is one of the first countries to use E-voting (over the internet voting) for general election. This D3.js based interactive bubble chart shows the profile of a typical electronic voter by allowing the viewer to explore how the fraction of electronic votes depends on different characteristics of candidates."
+    description: "Estonia is one of the first countries to use E-voting (over the internet voting) in general elections. This D3.js based interactive bubble chart shows the profile of a typical electronic voter. The viewer can explore how the fraction of electronic votes depends on different characteristics of the candidates."
 };
 
 projects.phd =  {
@@ -69,7 +69,7 @@ projects.phd =  {
     link: "https://dspace.ut.ee/bitstream/handle/10062/49900/reinson_tormi.pdf?sequence=1&isAllowed=y",
     visibleLink: "Link to thesis",
     tags: ["Science", "Data analysis", "Data visualization", "Public speaking", "Writing"],
-    description: "<p>I have a PhD degree in molecular virology. During my studies I clarified some important aspects of Human papillomavirus DNA replication that could play a role in causing cancer.</p><p>  I published <a href='https://www.ncbi.nlm.nih.gov/pubmed?term=(reinson%20t)%20AND%20(%222011%2F01%2F01%22%5BDate%20-%20Publication%5D%20%3A%20%222016%2F01%2F01%22%5BDate%20-%20Publication%5D)'>four scientific articles</a> and gave several presentations in international confernces. I performed and analyzed tons of experiments and wrote up the results.</p><p> It was an interesting job and invaluable experience.</p> "
+    description: "<p>I have a PhD degree in molecular virology. During my studies I clarified important aspects of Human papillomavirus DNA replication that could play a role in causing cancer.</p><p>  I have published <a href='https://www.ncbi.nlm.nih.gov/pubmed?term=(reinson%20t)%20AND%20(%222011%2F01%2F01%22%5BDate%20-%20Publication%5D%20%3A%20%222016%2F01%2F01%22%5BDate%20-%20Publication%5D)'>four scientific articles</a> and have given several presentations in international confernces. I performed and analyzed loads of experiments and wrote up the results.</p><p> It was an interesting job and invaluable experience.</p> "
 };
 
 projects.wines = {
@@ -78,7 +78,7 @@ projects.wines = {
     link: "http://htmlpreview.github.io/?https://raw.githubusercontent.com/reinson/UdacityProject3_Explore-and-Summarize-Data/master/Tormi_Reinson.html",
     visibleLink: "Link to analysis",
     tags: ["Data analysis", "Data visualization", "R", "Udacity"],
-    description: "The goal of this exploratory data analysis was to determine the physicochemical determinants of wine quality. It turned out that there is no single property or a compound that determines the quality of a wine. However, some factors, like alcohol percent and volatile acidity were moderately correlated with wine quality."
+    description: "The goal of this exploratory data analysis was to determine the physicochemical determinants of wine quality. It turned out that there is no single property or a compound that determines the quality of a wine. However, some factors, like alcohol percent (positive relationship) and volatile acidity (negative relationship) were moderately correlated with wine quality."
 };
 
 projects.machineLearning = {
